30315f13981fd9c7332c382d3390a1d20d07de4f
Hunter <hunter.baisden@gmail.com>
2023-12-20 19:40:59 -0600

    fixed up the helper function to make the poster

--- src/helper.py
+++ src/helper.py
@@ -12,50 +12,59 @@ import scipy.cluster
 import base64
 import io
 
-
 class Utility:
     def __init__(self, album):
         self.album = album
+        self.width = 740
+        self.height = 1200
+        self.below_pic_h = 710
+        self.margin = 50
+        self.text_color = self.album.text_color
+        self.background = self.album.background
 
     def buildPoster(self):
-        # create a blank canvas
-        width = 740
-        height = 1200
-        below_pic_h = 710
-        margin = 50
-        background = self.album.background
-        text_color = self.album.text_color
-        poster = Image.new(mode="RGBA", size=(width, height), color=background)
-
-        code_banner = return_banner(
-            self.album.album_id, background, text_color)
-        # add the album cover to the top
-        album_image_url = self.album.getCoverArt()[0]['url']
-        response = requests.get(album_image_url)
-        album_img = Image.open(BytesIO(response.content)).convert("RGBA")
-
-        # overlay the album cover
-        poster.paste(album_img, (margin, margin), album_img)
-
-        # overlay the code banner
-        poster.paste(code_banner, (440 - margin + 15, 1125,
-                     740 - margin + 15, 1200), code_banner)
-
-        # draw object creation
+        poster = self.create_poster(self.width, self.height, self.album.background)
+        album_img = self.fetch_album_cover(self.album.getCoverArt()[0]['url'])
+        self.overlay_album_cover(poster, album_img)
+        self.overlay_code_banner(poster)
         draw = ImageDraw.Draw(poster)
+        self.draw_artist_name(draw)
+        self.draw_album_name(draw)
+        self.draw_tracks(draw)
+        self.draw_release_date(draw)
+        self.draw_runtime(draw)
+        self.draw_label(draw)
+        self.draw_color_squares(draw, album_img)
+        return poster
 
-        # artist name text
+
+
+    def create_poster(self, width, height, background): #create the blank canvas of the poster
+        return Image.new(mode="RGBA", size=(width, height), color=background)
+
+    def fetch_album_cover(self, url): #return the image of the album cover
+        response = requests.get(url)
+        return Image.open(BytesIO(response.content)).convert("RGBA")
+
+    def overlay_album_cover(self, poster, album_img): #paste the album cover onto the image
+        poster.paste(album_img, (self.margin, self.margin), album_img)
+
+    def overlay_code_banner(self, poster):
+        code_banner = return_banner(self.album.album_id, self.album.background, self.text_color)
+        poster.paste(code_banner, (440 - self.margin + 15, 1125,
+                     740 - self.margin + 15, 1200), code_banner)
+
+    def draw_artist_name(self, draw): #draw the artists name on the poster
         artist_name = self.album.artist_name.upper()
         artist_font = ImageFont.truetype('static\Oswald-Medium.ttf', 30)
         # dimensions of artist text
         ascent, descent = artist_font.getmetrics()
         (w, baseline), (offset_x, offset_y) = artist_font.font.getsize(artist_name)
-
         # box = self.scaleDown(artist_name,width - margin - 200,below_pic_h,width - margin,below_pic_h + 30,artist_font,30)
-        draw.text((width - w - margin, below_pic_h + 30),
-                  artist_name, font=artist_font, fill=text_color)
+        draw.text((self.width - w - self.margin, self.below_pic_h + 30),
+                  artist_name, font=artist_font, fill=self.text_color)
 
-        # album name text
+    def draw_album_name(self, draw): #draw the name of the album on the poster
         album_name = self.album.album_name.upper()
         album_font = ImageFont.truetype('static\Oswald-Medium.ttf', 40)
 
@@ -66,20 +75,21 @@ class Utility:
         g = 0
         for string in album_list:
             (w, baseline), (offset_x, offset_y) = album_font.font.getsize(string)
-            draw.text((width - w - margin, below_pic_h + (offset_y) + (ascent -
-                      offset_y) + descent + g + 15),  string, font=album_font, fill=text_color)
+            draw.text((self.width - w - self.margin, self.below_pic_h + (offset_y) + (ascent -
+                      offset_y) + descent + g + 15),  string, font=album_font, fill=self.text_color)
             g += 45
 
-        # tracks text
+
+    def draw_tracks(self, draw): #put the tracks on the poster
         tracks = self.album.getTracks().values()
 
-        if (height - 50 - below_pic_h) > 35 * self.album.getNumTracks():
+        if (self.height - 50 - self.below_pic_h) > 35 * self.album.getNumTracks():
             increment = 35
             font = 30
         else:
             # taking the height, subtracting 50 for margin and then 710 for the starting height
-            increment = (height - 50 - below_pic_h)/self.album.getNumTracks()
-            font = int((height - 50 - below_pic_h) /
+            increment = (self.height - 50 - self.below_pic_h)/self.album.getNumTracks()
+            font = int((self.height - 50 - self.below_pic_h) /
                        self.album.getNumTracks()) - 5
 
         track_font = ImageFont.truetype('static\Oswald-Medium.ttf', font)
@@ -90,13 +100,15 @@ class Utility:
         for value in tracks:
             if len(tracks) <= 15:  # if the tracks are too long then ignore, otherwise truncate them
                 value = (value[:23] + '..') if len(value) > 25 else value
-            draw.text((margin, 710 + offset), str(tracknum) + space +
-                      value.upper(), font=track_font, fill=text_color)
+            draw.text((self.margin, 710 + offset), str(tracknum) + space +
+                      value.upper(), font=track_font, fill=self.text_color)
             offset = offset + increment
             tracknum = tracknum + 1
             if tracknum == 10:
                 space = ' '
 
+
+    def draw_release_date(self, draw): #put the release date on the poster
         # release date text
         date_string = self.album.getReleaseDate()
         date_font = ImageFont.truetype('static\Oswald-Medium.ttf', 30)
@@ -105,10 +117,11 @@ class Utility:
         ascent, descent = date_font.getmetrics()
         (w, baseline), (offset_x, offset_y) = date_font.font.getsize(date_string)
 
-        draw.text((width - w - margin, below_pic_h + 230),
-                  date_string, font=date_font, fill=text_color)
+        draw.text((self.width - w - self.margin, self.below_pic_h + 230),
+                  date_string, font=date_font, fill=self.text_color)
 
-        # Display runtime with release year
+    def draw_runtime(self, draw): #put the total runtime on the poster
+         # Display runtime with release year
         runtime_string = self.album.getRuntime()
         runtime_font = ImageFont.truetype('static\Oswald-Medium.ttf', 30)
 
@@ -116,9 +129,10 @@ class Utility:
         ascent, descent = runtime_font.getmetrics()
         (w, baseline), (offset_x, offset_y) = runtime_font.font.getsize(runtime_string)
 
-        draw.text((width - w - margin, below_pic_h + 270),
-                  runtime_string, font=runtime_font, fill=text_color)
+        draw.text((self.width - w - self.margin, self.below_pic_h + 270),
+                  runtime_string, font=runtime_font, fill=self.text_color)
 
+    def draw_label(self, draw):
         # label text
         # plit and take everything before first comma
         label_string = "Released by " + self.album.getLabel().split(',')[0]
@@ -132,11 +146,11 @@ class Utility:
         g = 0
         for string in label_list:
             (w, baseline), (offset_x, offset_y) = label_font.font.getsize(string)
-            draw.text((width - w - margin, below_pic_h + 310 + g),
-                      string, font=label_font, fill=text_color)
+            draw.text((self.width - w - self.margin, self.below_pic_h + 310 + g),
+                      string, font=label_font, fill=self.text_color)
             g += 30
 
-        # get color squares using helper function to get most vibrant colors of album image
+    def draw_color_squares(self, draw, album_img): #put the most common square colros on the poster
         colors = self.get_colors(album_img, 5, 250)
 
         offset = 0
@@ -144,15 +158,13 @@ class Utility:
         for color in colors:
             #draw.rectangle([(width - margin - offset, below_pic_h), (width -
                            #margin - offset - 30, below_pic_h + 30)], fill=color, outline=color)
-            draw.rectangle([(width - margin - offset - 30, below_pic_h), 
-                (width - margin - offset, below_pic_h + 30)], 
-                fill=color, outline=color)
+            draw.rectangle([(self.width - self.margin - offset - 30, self.below_pic_h), 
+                (self.width - self.margin - offset, self.below_pic_h + 30)], 
+                fill=color, outline=color) #weird fix that needed to be made for some reason. Not sure how this wasnt erroring before
 
             offset += spacing
 
-        return poster
-
-    def get_colors(self, image, numcolors=6, resize=150):
+    def get_colors(self, image, numcolors=6, resize=150): #helper function to return the dominate colors in an image.
         # Resize image to speed up processing
         img = image.copy()
         img.thumbnail((resize, resize))
@@ -171,7 +183,7 @@ class Utility:
 
         return colors
 
-    def encodeImage(self, image):
+    def encodeImage(self, image): #encode the image to be sent to htrml to be displayed
         data = io.BytesIO()
         image.save(data, "PNG")
         encoded_img_data = base64.b64encode(data.getvalue())
@@ -179,7 +191,7 @@ class Utility:
         img_data = f"data:image/png;base64,{decoded_img}"
         return img_data
 
-    def scaleDown(self, text, x1, y1, x2, y2, font, font_size):
+    def scaleDown(self, text, x1, y1, x2, y2, font, font_size): #used to scale down an image if need be
         im = Image.new("RGB", (x2-x1, y2-y1), "#fff")
         box = ((x1, y1, x2, y2))
         print(box)
@@ -192,4 +204,5 @@ class Utility:
             size = font.getsize_multiline(text)
             font_size -= 1
         draw.multiline_text((box[0], box[1]), text, "#000", font)
-        im.show()
+
+
