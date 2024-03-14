a705504e31678b2cb7da1e25756fcf5507e49d9a
Hunter <hunter.baisden@gmail.com>
2024-03-14 17:20:38 -0500

    local.py changes

--- local.py
+++ local.py
@@ -1,10 +1,10 @@
 from src.album import Album
 from src.helper import Utility
 
-artist = "berhana"
-album = "han"
+artist = "berhana" #change the artist here
+album = "han" #change the album here 
 
 album = Album(artist,album)
-album.setColors("#FFFFFF","#000000")
+album.setColors("#FFFFFF","#000000") # change the background and text colors here, respectively
 poster = Utility(album).buildPoster()
 poster.show()
